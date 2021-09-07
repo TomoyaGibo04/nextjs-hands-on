cfab5927689ccbb127c47d3c5887271856852833
TomoyaGibo04 <ic181212@edu.okinawa-ct.ac.jp>
2021-09-07 16:56:28 +0900

    テキスト変更

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 const About = () => {
-    return (
-        <>
-        <div>about page</div>
-        <Link href='/'>Home</Link>
+  return (
+    <>
+      <div>about page</div>
+      <Link href="/">Home</Link>
+      <br></br>
 
-        <Button variant="contained" color="primary">
-            Hello!
-        </Button>
+      <Button variant="contained" color="primary">
+        Hello!
+      </Button>
     </>
-    )
+  );
 };
 
 export default About;
