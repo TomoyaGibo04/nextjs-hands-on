44e0c8554a7b474670a6ea4cfb275b19667651a5
TomoyaGibo04 <ic181212@edu.okinawa-ct.ac.jp>
2021-09-07 11:31:29 +0900

    callAPI

--- pages/apitest.js
+++ pages/apitest.js
@@ -10,9 +10,8 @@ const Example = ({ gourmet }) => {
   );
 };
 
-// export async function getStaticProps() {
+// データ取得準備
 export async function getServerSideProps() {
-  // 取得テスト
   const { serverRuntimeConfig } = getConfig();
   const { HOTPEPPER_API_KEY } = serverRuntimeConfig;
   const gourmetUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${HOTPEPPER_API_KEY}&large_area=Z011&format=json`;
