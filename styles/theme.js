42b1f8e290c5484abdad98159678a4573ef84a09
TomoyaGibo04 <ic181212@edu.okinawa-ct.ac.jp>
2021-09-07 17:00:39 +0900

    change color

--- styles/theme.js
+++ styles/theme.js
@@ -1,14 +1,14 @@
 import { createTheme } from '@material-ui/core/styles';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import red from '@material-ui/core/colors/red';
+import { blueGrey } from '@material-ui/core/colors';
 
-export const palette = {
-};
+export const palette = {};
 
 const theme = createTheme({
   palette: {
     primary: deepPurple,
-    secondary: red,
+    secondary: blueGrey,
   },
 });
 
