395feabfae508e6e8a1a7d8bc42482a0bf28ba5f
TomoyaGibo04 <ic181212@edu.okinawa-ct.ac.jp>
2021-09-07 11:34:18 +0900

    EnvironmentVariable

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-const { NODE_ENV, APP_NAME, HOTPEPPER_API_KEY } = process.env;
+diff --git a/next.config.js b/next.config.js
+index 81c6130..c2089f8 100644
+--- a/next.config.js
++++ b/next.config.js
+@@ -1,9 +1,10 @@
+-const { NODE_ENV, APP_NAME } = process.env;
++const { NODE_ENV, APP_NAME, HOTPEPPER_API_KEY } = process.env;
 
-const nextConfig = {
-  publicRuntimeConfig: {
-    env: NODE_ENV,
-    APP_NAME,
-  },
-  serverRuntimeConfig: {
-    HOTPEPPER_API_KEY,
-  },
-};
-
-module.exports = nextConfig;
+ const nextConfig = {
+   publicRuntimeConfig: {
+     env: NODE_ENV,
+     APP_NAME,
++  },
++  serverRuntimeConfig: {
++    HOTPEPPER_API_KEY,
+   },
+ };
